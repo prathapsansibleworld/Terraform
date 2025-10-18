7460fe6b73df2fd62d6ecd9d1ad0474b32668354
prathapreddy <prathapreddy.s18@gmail.com>
2025-10-18 17:50:38 +0530

    Terraform

---
+++ count/Ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+  #count = 2
+    count = length(var.instances)
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.alltraffic_allow.id]
+    tags = {
+        Name = var.instances[count.index]
+        Terraform = "true"
+        Project = "roboshop"
+    }
+}
+
+resource "aws_security_group" "alltraffic_allow" {
+  name   = "alltraffic-allow"
+
+  egress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  ingress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  tags = {
+    Name = "alltrafic-allow"
+  }
+
+}
---
+++ count/variables.tf
@@ -0,0 +1,10 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", ]
+}
+variable "zone_id" {
+    default = "Z0275509EJCTOUIAQQEV"
+}
+
+variable "domain_name" {
+    default = "playandlearn.fun"
+}
